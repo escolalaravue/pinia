b609237539618001d112e1c4a0a3ea68a9300fc3
Tiago Matos <tiago@tiagomatos.com>
2024-04-09 09:20:33 +1000

    Atualizando o state

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,26 @@
   <div>
     <h1>Curso de Pinia</h1>
     <h4>User store</h4>
+    <input type="text" v-model="userStore.user.first_name">
     <pre>
-      {{ userStore }}
-    </pre>
-    <pre>
-      {{ fullName }}
+      {{ userStore.user }}
     </pre>
     <br><br>
     <h4>Cart store</h4>
-    <pre>{{products}}</pre>
+    <pre>{{cartStore.products}}</pre>
   </div>
 </template>
 
-<script>
-import {mapState} from 'pinia';
+<script setup>
 import {useUserStore} from './store/user.js';
 import {useCartStore} from './store/cart.js';
 
-export default {
-  computed: {
-    ...mapState(useUserStore, {
-      userStore: 'user',
-      fullName: store => `${store.user.first_name} ${store.user.last_name}`,
-    }),
-    ...mapState(useCartStore, ['products'])
-  }
-}
+const userStore = useUserStore();
+const cartStore = useCartStore();
+
+cartStore.products[0].name = 'Iphone 15'
+cartStore.products.push({
+  name: 'iMac',
+  amount: 200
+},)
 </script>
