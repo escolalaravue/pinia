20c012a008ba8b1c236fdf704eca6081041f8cf0
Tiago Matos <tiago@tiagomatos.com>
2024-04-09 10:25:18 +1000

    Observando o state

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div>
     <h1>Curso de Pinia</h1>
     <h4>User store</h4>
+    <input type="text" v-model="userStore.firstName"> <br>
+    <input type="text" v-model="userStore.lastName"> <br>
     {{ userStore.firstName }} <br>
     {{ userStore.lastName }} <br>
     {{ userStore.age }} <br>
@@ -17,9 +19,18 @@
 <script setup>
 import {useUserStore} from './store/user.js';
 import {useCartStore} from './store/cart.js';
+import {watch} from 'vue';
 
 const userStore = useUserStore();
 const cartStore = useCartStore();
+
+watch(userStore, (vl) => {
+  console.log(vl)
+})
+
+watch(() => userStore.firstName, (vl) => {
+  console.log(vl)
+})
 
 userStore.$patch({
   firstName: 'Tiago',
